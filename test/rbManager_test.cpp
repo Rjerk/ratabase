95279093b3b781d7b155bbb2465796790be618e1
Rjerk <460139918@qq.com>
2017-04-22 14:49:53 +0800

    test rbmanager

---
+++ test/rbManager_test.cpp
@@ -0,0 +1,69 @@
+#include "../ratabaseManager.h"
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    RatabaseManager manager;
+
+    cout << "create movie" << endl;
+    cmd_v create = {
+        .type = cmd_type::CREATE,
+        .cmd_name = "create",
+        .obj_name = "movie",
+        .params = {}
+    };
+    bool ret = manager.manageDB(create);
+    assert(ret);
+
+    cout << "select movie" << endl;
+    cmd_v select = {
+        .type = cmd_type::SELECT,
+        .cmd_name = "select",
+        .obj_name = "movie",
+        .params = {}
+    };
+    ret = manager.manageDB(select);
+    assert(ret);
+
+    cout << "display db" << endl;
+    cmd_v display = {
+        .type = cmd_type::DISPLAY,
+        .cmd_name = "",
+        .obj_name = "",
+        .params = {}
+    };
+    ret = manager.manageDB(display);
+    assert(ret);
+
+    cout << "delete movie" << endl;
+    cmd_v delet = {
+        .type = cmd_type::DELETE,
+        .cmd_name = "delete",
+        .obj_name = "movie",
+        .params = {}
+    };
+    ret = manager.manageDB(delet);
+    assert(ret);
+
+    cout << "select movie" << endl;
+    ret = manager.manageDB(select);
+    assert(ret == false);
+
+    cout << "display db" << endl;
+    ret = manager.manageDB(display);
+    assert(ret);
+
+    cout << "delete default_db" << endl;
+    delet.obj_name = "default_db";
+    ret = manager.manageDB(delet);
+    assert(ret);
+
+    cout << "display db" << endl;
+    ret = manager.manageDB(display);
+    assert(ret == false);
+
+}
---
+++ test/rbManager_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -O -Wall -std=c++11 ../ratabase.cpp ../hash.cpp ../value.cpp ../ratabaseManager.cpp rbManager_test.cpp -o rbManager_test
