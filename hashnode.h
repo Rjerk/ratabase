742fd9340f93233803dfb276c033438032fc3e88
Rjerk <460139918@qq.com>
2017-05-06 16:37:12 +0800

    add hashnode's value storage

--- hashnode.h
+++ hashnode.h
@@ -4,23 +4,34 @@
 #include "value.h"
 #include <cstddef>
 #include <iostream>
+#include <cstring>
 
 class Hashnode {
 public:
     Hashnode(): key(nullptr), next(nullptr) { }
     Hashnode(const char* k, const Value& v):
-        key(k), value(v), next(nullptr) { }
-
+        key(new char[strlen(k)+1]),
+        value(v),
+        next(nullptr)
+    {
+        strncpy(key, k, strlen(k));
+        key[strlen(k)] = '\0';
+    }
+    ~Hashnode()
+    {
+        delete [] key;
+        value.freeValue();
+    }
     Hashnode(const Hashnode&) = delete;
     Hashnode& operator=(const Hashnode&) = delete;
 
-    const char* getKey() const { return key;}
+    const char* getKey() const { return key; }
     Value getValue() const { return value; }
     void setValue(const Value& v) { value = v; }
     Hashnode* getNext() const { return next; }
     void setNext(Hashnode* n) { next = n; }
 private:
-    const char* key;
+    char* key;
     Value value;
     Hashnode* next;
 };
