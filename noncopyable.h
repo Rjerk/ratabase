ceac940ea20cd53aa4dd62ba0cf586040e0dd022
Rjerk <460139918@qq.com>
2017-08-16 21:35:49 +0800

    noncopyable

---
+++ noncopyable.h
@@ -0,0 +1,14 @@
+#ifndef NONCOPYABLE_H
+#define NONCOPYABLE_H
+
+class noncopyable {
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+private:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable operator=(const noncopyable&) = delete;
+
+};
+
+#endif
